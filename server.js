e7aa1bb667f068fb4348a4bcff54844f1ff18f97
deepg2018 <deeragon.goel@gmail.com>
2016-09-26 01:32:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
