f77df9140759be44e901f2a46ce9d96b0444e2ea
deepg2018 <deeragon.goel@gmail.com>
2016-09-26 11:42:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
+
+
+button.oneclick=function()
+{
+  var request=new XMLhttpRequest();
+  
+  request.onreadystatechange=function()
+  {
+      if(request.readyState===XMLhttpRequest.DONE)
+      {
+          if(request.status===200)
+          {
+              var counter=request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+          }
+      }
+  }
+  
+  request.open('GET','http://http://deepg2018.imad.hasura-app.io/counter',true);
+  request.send(null);
+ 
+ };
